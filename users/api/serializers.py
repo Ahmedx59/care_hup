0551142fa85682e572ee7332c48088bcc1769942
elawady740 <ahmedelawady740@gmail.com>
2025-04-15 21:56:22 +0200

    update

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -99,8 +99,8 @@ class SignUpDoctorNurseSerializer(serializers.ModelSerializer):
         price = validated_data.pop('price')
         specialty = validated_data.pop('specialty')
         city = validated_data.pop('city')
-        services = validated_data.pop('services')
         card = validated_data.pop('card')
+        services = validated_data.pop('services')
 
 
         send_mail(
@@ -119,7 +119,6 @@ class SignUpDoctorNurseSerializer(serializers.ModelSerializer):
         user_profile.specialty = specialty
         user_profile.city = city
         user_profile.card = card
-
         user_profile.services = services
 
         user_profile.save()
