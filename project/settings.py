ab0c5c31c15f3494f616dde930358ff185280aaa
Ahmedx59 <Ahmedx59@gmail.com>
2025-07-12 22:05:16 +0300

    update send email in setting (mail in phone email)

--- project/settings.py
+++ project/settings.py
@@ -173,14 +173,14 @@ SIMPLE_JWT = {
     }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'elagameahmed5@gmail.com'
-# EMAIL_HOST_PASSWORD = 'zecyhvcogywrdety'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'elagameahmed5@gmail.com'
+EMAIL_HOST_PASSWORD = 'ynfmkmecxgugtvlw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 
 # إعدادات Celery
